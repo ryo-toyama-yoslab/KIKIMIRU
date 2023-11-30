3edaf7896525320f80c7fa48facb879f6937c8d1
ryo-toyama-yoslab <toyama.ryo@g.wakayama-u.jp>
2023-11-30 19:59:04 +0900

    通知なしモードの選択肢を追加

--- app/src/main/java/com/example/toyamaryo/bladesampleapp/CheckoutRunMode.java
+++ app/src/main/java/com/example/toyamaryo/bladesampleapp/CheckoutRunMode.java
@@ -11,9 +11,9 @@ import android.widget.Switch;
 
 public class CheckoutRunMode extends Activity {
 
-    public Switch s1, s2;
+    public Switch s1, s2, s3;
     public Button checkout_btn;
-    int mode = 0; // mode1 : 機械通知音, mode2 : 音声通知
+    int mode = 0; // mode1 : 通知音無し, mode2 : 機械通知音, mode3 : 音声通知
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +24,17 @@ public class CheckoutRunMode extends Activity {
         checkout_btn = findViewById(R.id.setting_button);
         s1 = findViewById(R.id.run_mode_1_switch);
         s2 = findViewById(R.id.run_mode_2_switch);
+        s3 = findViewById(R.id.run_mode_3_switch);
 
         checkout_btn.setEnabled(false);//設定ボタンを非アクティブ
         s1.setText("未選択");
         s2.setText("未選択");
+        s3.setText("未選択");
 
         Intent intentMain = getIntent();
+
+        // 一番上のボタンにフォーカスしておく
+        s1.requestFocus();
 
         Log.d("現在のモード",Integer.toString(intentMain.getIntExtra("experimentMode",0)));
         mode = intentMain.getIntExtra("experimentMode",0);
@@ -38,6 +43,10 @@ public class CheckoutRunMode extends Activity {
             s1.setText("選択");
             checkout_btn.setEnabled(true);
         }else if(mode == 2){
+            s2.setChecked(true);
+            s2.setText("選択");
+            checkout_btn.setEnabled(true);
+        }else if(mode == 3){
             s2.setChecked(true);
             s2.setText("選択");
             checkout_btn.setEnabled(true);
@@ -53,9 +62,11 @@ public class CheckoutRunMode extends Activity {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
-                    if(s2.isChecked()) {
+                    if(s2.isChecked() || s3.isChecked()) {
                         s2.setChecked(false);
                         s2.setText("未選択");
+                        s3.setChecked(false);
+                        s3.setText("未選択");
                     }
                     s1.setChecked(true);
                     s1.setText("選択");
@@ -75,9 +86,11 @@ public class CheckoutRunMode extends Activity {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
-                    if(s1.isChecked()) {
+                    if(s1.isChecked() || s3.isChecked()) {
                         s1.setChecked(false);
                         s1.setText("未選択");
+                        s3.setChecked(false);
+                        s3.setText("未選択");
                     }
                     s2.setChecked(true);
                     s2.setText("選択");
@@ -90,6 +103,30 @@ public class CheckoutRunMode extends Activity {
                 }
             }
         });
+
+        //レベル3(経験度低の医療従事者)を選択した場合
+        s3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    if(s1.isChecked() || s2.isChecked()) {
+                        s1.setChecked(false);
+                        s1.setText("未選択");
+                        s2.setChecked(false);
+                        s2.setText("未選択");
+                    }
+                    s3.setChecked(true);
+                    s3.setText("選択");
+                    mode = 3;
+                    checkout_btn.setEnabled(true);
+                } else {
+                    s3.setChecked(false);
+                    s3.setText("未選択");
+                    checkout_btn.setEnabled(false);
+                }
+            }
+        });
+
 
         //メイン画面(MainActivityに遷移するボタン)
         checkout_btn.setOnClickListener(new View.OnClickListener() {
